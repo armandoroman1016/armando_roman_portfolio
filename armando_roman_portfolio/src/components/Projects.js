44aee97db6a635e413e0d193e2b611b0c3a7517a
Armando Roman <armandoroman1016@gmail.com>
2019-09-04 18:54:46 -0700

    projects page styled

--- armando_roman_portfolio/src/components/Projects.js
+++ armando_roman_portfolio/src/components/Projects.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { projects } from '../data/projects'
 import NavBar from './NavBar'
+import github from '../assests/images/github.png'
+import computer from '../assests/images/computer.png'
+
 const Projects = props => {
 
     return(
         <section className = 'projects-section'>
         <NavBar path = {props.location.pathname}/>
-        <h1>The Work</h1>
         <div className = 'projects-container'>
+        <h1>The Work</h1>
             {projects.map( project => {
                 return(
                     <div key = {project.id} className = 'project-container'>
@@ -16,12 +19,10 @@ const Projects = props => {
                         <p className = 'project-description'>{project.projectDescription}</p>
                         <div className = 'project-links'>
                             <div className = 'project-link'>
-                                
-                                <a href = {project.githubUrl}>View The Code On Github</a>
+                                <a href = {project.githubUrl}><img src = {github} alt = 'github icon' className = 'icon'/>View The Code On Github</a>
                             </div>
                             <div className = 'project-link'>
-                                
-                                <a href = {project.projectUrl}>View This Project</a>
+                                <a href = {project.projectUrl}><img src = {computer} alt = 'computer icon' className = 'icon' />View This Project</a>
                             </div>
                         </div>
                     </div>
