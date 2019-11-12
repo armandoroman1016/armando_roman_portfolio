7307e8264ef48dc05779293fa173ff7b9f354afa
Armando Roman <armandoroman1016@gmail.com>
2019-11-11 22:22:54 -0800

    copy added to about page

--- armando_roman_portfolio/src/components/About.js
+++ armando_roman_portfolio/src/components/About.js
@@ -4,10 +4,11 @@ import NavBar from './NavBar'
 const About = props =>{
     return(
         <section className = 'about-container page'>
-            <div clasName = 'about-text'>
+            <div className = 'about-text'>
                 <h2>Who Is Armando Roman<span> ?</span></h2>
                 <p>
-                    Armando Roman is a Full Stack Web Developer that truly enjoys building products that will benefit many.
+                    Armando Roman is a Full Stack Web Developer that truly enjoys building products that will benefit many people.
+                    
                 </p>
             </div>
             <span className = 'background-text javascript'>JAVASCRIPT</span>
