3f3cf081e56c379d2a118deb140879a6d45e2206
Armando Roman <armandoroman1016@gmail.com>
2019-11-12 18:33:15 -0800

    landing page responsiveness just about complete

--- armando_roman_portfolio/src/components/NavBar.js
+++ armando_roman_portfolio/src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const NavBar = props => {
+
+    
     return (
         <div className = 'nav-bar'  >
             <ul className = 'nav-links'>
