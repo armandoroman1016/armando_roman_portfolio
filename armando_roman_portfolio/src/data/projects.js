4b10337b7923d1a873c1d9454a1a1f1f3cb9c582
Armando Roman <armandoroman1016@gmail.com>
2019-11-21 15:54:40 -0800

    mission control image added, copy needed for projects

--- armando_roman_portfolio/src/data/projects.js
+++ armando_roman_portfolio/src/data/projects.js
@@ -1,5 +1,5 @@
 import PartyPlanner from '../assests/images/party_planner_iphone.png'
-
+import MissionControl from '../assests/images/use-mission-control-com.png'
 
 export const projects = [
     {
@@ -10,5 +10,14 @@ export const projects = [
         projectDescription: 'This project was built using ReactJS and Redux was implemented for state management, the data consumed in this app is coming from Open Brewery DB Api using axios.',
         projectUrl: 'https://elastic-jennings-52d92d.netlify.com/login',
         githubUrl: 'https://github.com/build-week-party-planner/Front-End'
+    },
+    {
+        id: 3,
+        hasMobileMock: false,
+        projectName: "Mission Control",
+        projectImg: MissionControl,
+        projectDescription: 'This project was built using ReactJS and Redux was implemented for state management, the data consumed in this app is coming from Open Brewery DB Api using axios.',
+        projectUrl: 'https://use-mission-control.com/login',
+        githubUrl: 'https://github.com/orgs/Lambda-School-Labs/teams/labs-17-mission-control/repositories'
     }
 ]
